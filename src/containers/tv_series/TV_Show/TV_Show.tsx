384a33b83e9e8716a3b6b5ba15cd18f80ef18419
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-30 18:59:10 +0600

    add loading

--- src/containers/tv_series/TV_Show/TV_Show.tsx
+++ src/containers/tv_series/TV_Show/TV_Show.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks/redux';
 import {
@@ -6,12 +6,14 @@ import {
   selectShow,
 } from '../../../redux/features/tvSeries/tvSeriesSlice';
 import './TV_Show.css';
+import Loading from '../../../UI/Loading/Loading';
 
 const TV_Show = () => {
   const dispatch = useAppDispatch();
   const show = useAppSelector(selectShow);
   const params = useParams<{ idShow: string }>();
   let summery: null | string = null;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   if (show) {
     summery = show.summary;
@@ -19,60 +21,70 @@ const TV_Show = () => {
 
   useEffect(() => {
     if (params.idShow) {
+      setIsLoading(true);
       dispatch(getTVShowByQ(params.idShow));
+      setIsLoading(false);
     }
   }, [params.idShow]);
 
   return (
     <>
-      {show && (
-        <div className="show-block">
-          <div className="show">
-            <div className="show-image">
-              <img src={show.image.original} alt={show.name} />
-            </div>
-            <div className="show-info">
-              <p className="show-name">{show.name}</p>
+      {isLoading ? (
+        <div className="centered-block">
+          <Loading />
+        </div>
+      ) : (
+        <>
+          {show && (
+            <div className="show-block">
+              <div className="show">
+                <div className="show-image">
+                  <img src={show.image.original} alt={show.name} />
+                </div>
+                <div className="show-info">
+                  <p className="show-name">{show.name}</p>
 
-              <div className="about-show">
-                <p className="about-show-title">About Show</p>
+                  <div className="about-show">
+                    <p className="about-show-title">About Show</p>
 
-                <div className="about-show-info">
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Year of production:</p>
-                    <p>{show.premiered}</p>
-                  </div>
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Language:</p>
-                    <p>{show.language}</p>
-                  </div>
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Rating:</p>
-                    <p>{show.rating.average}</p>
-                  </div>
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Status:</p>
-                    <p>{show.status}</p>
-                  </div>
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Runtime:</p>
-                    <p>{show.runtime}</p>
-                  </div>
-                  <div className="about-show-labels">
-                    <p className="about-show-label">Genre:</p>
-                    <p>{show.genres.join(', ')}</p>
+                    <div className="about-show-info">
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Year of production:</p>
+                        <p>{show.premiered}</p>
+                      </div>
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Language:</p>
+                        <p>{show.language}</p>
+                      </div>
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Rating:</p>
+                        <p>{show.rating.average}</p>
+                      </div>
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Status:</p>
+                        <p>{show.status}</p>
+                      </div>
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Runtime:</p>
+                        <p>{show.runtime}</p>
+                      </div>
+                      <div className="about-show-labels">
+                        <p className="about-show-label">Genre:</p>
+                        <p>{show.genres.join(', ')}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
+              {summery && (
+                <div
+                  dangerouslySetInnerHTML={{ __html: summery }}
+                  className="show-summery"
+                />
+              )}
             </div>
-          </div>
-          {summery && (
-            <div
-              dangerouslySetInnerHTML={{ __html: summery }}
-              className="show-summery"
-            />
           )}
-        </div>
+        </>
       )}
     </>
   );
